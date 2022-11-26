d729e700e8534c59693a9ab9bbf2d2302a988918
SaxophOnyx <kysbeg@gmail.com>
2022-11-26 10:16:59 +0300

    Completed lab4

---
+++ Example/FileToFileGeneration/FileToFileGeneratorOptions.cs
@@ -0,0 +1,31 @@
+﻿namespace Example.FileToFileGeneration
+{
+    public class FileToFileGeneratorOptions
+    {
+        public int MaxRead { get; init; }
+
+        public int MaxProcess { get; init; }
+
+        public int MaxWrite { get; init; }
+
+        public static FileToFileGeneratorOptions Default
+        {
+            get { return _default; }
+        }
+
+        private static FileToFileGeneratorOptions _default = new FileToFileGeneratorOptions()
+        {
+            MaxRead = 5,
+            MaxProcess = 5,
+            MaxWrite = 5
+        };
+
+
+        public FileToFileGeneratorOptions()
+        {
+            MaxRead = 5;
+            MaxProcess = 5;
+            MaxWrite = 5;
+        }
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,29 @@
+﻿using TestsGeneration.Generators;
+using TestsGeneration.Parsers;
+using Example.FileToFileGeneration;
+
+namespace Example
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string resultDirPath = @"D:\files\results";
+            List<string> files = new List<string>()
+            {
+                @"D:\files\Input.cs",
+                @"D:\files\DoubleInput.cs"
+            };
+
+            FileToFileGenerator generator = new(
+                new Parser(),
+                new xUnitTestGenerator(),
+                FileToFileGeneratorOptions.Default);
+
+            await generator.GenerateTestsAsync(files, resultDirPath);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
